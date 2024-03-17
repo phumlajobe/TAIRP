d3a517c3e8c5d6ce71910785a8f355cf1909e591
phumlajobe <phumlasj@gmail.com>
2024-03-18 00:16:21 +0200

    Added portfolio response using javaSCript

--- Level1/Portfolio/src/script.js
+++ Level1/Portfolio/src/script.js
@@ -1,24 +1,48 @@
+function controlHover(contentId){
+    // adding the hover to be default
+    let activeLink = document.querySelector(`.profile-nav a[href="${contentId}"]`);
+    let allLinks = document.querySelectorAll('.profile-nav li');
+    activeLink.parentElement.classList.add("hovered");
+
+    // Remove "hovered" class from all list items except the one containing the active link
+    allLinks.forEach(link => {
+        if (link !== activeLink.parentElement) {
+            link.classList.remove("hovered");
+        }
+    });
+}
+
+
 function displayContent(contentId){
-    console.log(contentId);
+    controlHover(contentId);
     let display = document.querySelector(contentId);
     let contentElement = document.querySelector("#portfolio-content");
     contentElement.innerHTML = display.innerHTML;
+
 }
 
-let navElement = document.querySelectorAll("#profile-list a");
-navElement.forEach(function(link){
+
+function findActiveLink(link){
     link.addEventListener("click", function(event) {
         event.preventDefault();
+        console.log(link.innerHTML)
     
         // Get the target content ID from the link's href attribute
         const targetContentId = link.getAttribute("href");
-        displayContent(targetContentId);
-        
+        displayContent(targetContentId);   
 
     });
+}
 
-});
+
+let navElement = document.querySelectorAll("#profile-list a");
+navElement.forEach(findActiveLink);
 
 let allContent = document.querySelector("#content");
 allContent.style.display = "none";
-displayContent("#about");
+
+displayContent("#about")
+
+function redirectToPortfolio() {
+    window.location.href = "#portfolio";
+}
